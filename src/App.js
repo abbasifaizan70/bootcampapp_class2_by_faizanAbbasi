0bd817594968489d96bbf02bbd8c1c33dfb3091a
Faizan Shoukat Abbasi <58352279+abbasifaizan70@users.noreply.github.com>
2020-07-12 20:05:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
         <h1> App Develop by REACT Faian Abbasi   </h1>
-    </div>
+    </div>  
   );
 }
 
